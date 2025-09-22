7ba831a5ad055cf22bb1aff8090810a03525ca38
CharlesLawson <charles.thomas.lawson@gmail.com>
2025-09-22 16:25:59 -0400

    Edited app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Amplify} from "aws-amplify";
+//import {Amplify} from "aws-amplify";
 
 function App() {
   const [count, setCount] = useState(0)
